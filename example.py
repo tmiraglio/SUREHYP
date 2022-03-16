29a6c7b0f327eb1cb4bcdc139bb35271f6ff857f
Babylas83700 <thomas.miraglio@gmail.com>
2022-03-16 09:36:10 -0700

    fix

--- example.py
+++ example.py
@@ -13,56 +13,56 @@ import surehyp.atmoCorrection
 
 def preprocess_radiance(fname,pathToL1Rmetadata,pathToL1Rimages,pathToL1Timages,pathToL1TimagesFiltered,pathOut,nameOut,destripingMethod='Pal',localDestriping=False,smileCorrectionOrder=2,checkSmile=False):    
     print('concatenate the L1T image')
-    preprocess.processImage(fname,pathToL1Timages,pathToL1TimagesFiltered)
+    surehyp.preprocess.processImage(fname,pathToL1Timages,pathToL1TimagesFiltered)
 
     print('read the L1R image')
-    arrayL1R=preprocess.readL1R(pathToL1Rimages+fname+'/',fname)
+    arrayL1R=surehyp.preprocess.readL1R(pathToL1Rimages+fname+'/',fname)
 
     print('get the L1R image parameters')
-    metadata,bands,fwhms=preprocess.getImageMetadata(pathToL1Rimages+fname+'/',fname)
+    metadata,bands,fwhms=surehyp.preprocess.getImageMetadata(pathToL1Rimages+fname+'/',fname)
 
     print('separates VNIR and SWIR')
-    VNIR,VNIRb,VNIRfwhm,SWIR,SWIRb,SWIRfwhm=preprocess.separating(arrayL1R,bands,fwhms)
+    VNIR,VNIRb,VNIRfwhm,SWIR,SWIRb,SWIRfwhm=surehyp.preprocess.separating(arrayL1R,bands,fwhms)
 
     print('converts DN to radiance')
-    VNIR,SWIR=preprocess.DN2Radiance(VNIR,SWIR)
+    VNIR,SWIR=surehyp.preprocess.DN2Radiance(VNIR,SWIR)
 
     print('aligning VNIR and SWIR, part 1')
-    VNIR,SWIR=preprocess.alignSWIR2VNIRpart1(VNIR,SWIR)
+    VNIR,SWIR=surehyp.preprocess.alignSWIR2VNIRpart1(VNIR,SWIR)
 
     print('desmiling')
-    VNIR=preprocess.smileCorrectionAll(VNIR,smileCorrectionOrder,check=checkSmile)
-    SWIR=preprocess.smileCorrectionAll(SWIR,smileCorrectionOrder,check=checkSmile)
+    VNIR=surehyp.preprocess.smileCorrectionAll(VNIR,smileCorrectionOrder,check=checkSmile)
+    SWIR=surehyp.preprocess.smileCorrectionAll(SWIR,smileCorrectionOrder,check=checkSmile)
 
     if destripingMethod=='Datt':
         print('destriping - Datt (2003)')
-        VNIR=preprocess.destriping(VNIR,'VNIR',0.11)
-        SWIR=preprocess.destriping(SWIR,'SWIR',0.11)
+        VNIR=surehyp.preprocess.destriping(VNIR,'VNIR',0.11)
+        SWIR=surehyp.preprocess.destriping(SWIR,'SWIR',0.11)
     elif destripingMethod=='Pal':
         print('destriping -  Pal et al. (2020)')
-        VNIR,nc=preprocess.destriping_quadratic(VNIR)
+        VNIR,nc=surehyp.preprocess.destriping_quadratic(VNIR)
         if localDestriping==True:
-            VNIR=preprocess.destriping_local(VNIR,nc)
-        SWIR,nc=preprocess.destriping_quadratic(SWIR)
+            VNIR=surehyp.preprocess.destriping_local(VNIR,nc)
+        SWIR,nc=surehyp.preprocess.destriping_quadratic(SWIR)
         if localDestriping==True:
-            SWIR=preprocess.destriping_local(SWIR,nc)
+            SWIR=surehyp.preprocess.destriping_local(SWIR,nc)
     else:
         print('no destriping method selected -> no destriping')
 
     #print('aligning VNIR and SWIR, part 2')
-    #VNIR,SWIR=preprocess.alignSWIR2VNIRpart2(VNIR,VNIRb,SWIR,SWIRb)
+    #VNIR,SWIR=surehyp.preprocess.alignSWIR2VNIRpart2(VNIR,VNIRb,SWIR,SWIRb)
 
     print('assemble VNIR and SWIR')
-    arrayL1R,wavelengths,fwhms=preprocess.concatenateImages(VNIR,VNIRb,VNIRfwhm,SWIR,SWIRb,SWIRfwhm)
+    arrayL1R,wavelengths,fwhms=surehyp.preprocess.concatenateImages(VNIR,VNIRb,VNIRfwhm,SWIR,SWIRb,SWIRfwhm)
 
     print('smooth the cirrus bands for later thin cirrus removal') #may be necessary for Hyperion as an incomplete destriping for this band would then affect every other band during the thin cirrus removal
-    arrayL1R=preprocess.smoothCirrusBand(arrayL1R,wavelengths)
+    arrayL1R=surehyp.preprocess.smoothCirrusBand(arrayL1R,wavelengths)
 
     print('georeference the corrected L1R data using the L1T data')
-    arrayL1Rgeoreferenced, metadataGeoreferenced=preprocess.georeferencing(arrayL1R,pathToL1TimagesFiltered,fname)
+    arrayL1Rgeoreferenced, metadataGeoreferenced=surehyp.preprocess.georeferencing(arrayL1R,pathToL1TimagesFiltered,fname)
 
     print('save the processed image as an ENVI file')
-    preprocess.savePreprocessedL1R(arrayL1Rgeoreferenced,wavelengths,fwhms,metadataGeoreferenced,pathToL1Rimages,pathToL1Rmetadata,metadata,fname,pathOut+nameOut)
+    surehyp.preprocess.savePreprocessedL1R(arrayL1Rgeoreferenced,wavelengths,fwhms,metadataGeoreferenced,pathToL1Rimages,pathToL1Rmetadata,metadata,fname,pathOut+nameOut)
 
     for f in os.listdir(pathOut):
         if (fname in f) and ('_tmp' in f):
@@ -72,7 +72,7 @@ def preprocess_radiance(fname,pathToL1Rmetadata,pathToL1Rimages,pathToL1Timages,
 
 def atmosphericCorrection(pathToRadianceImage,pathToOutImage,stepAltit=1,stepTilt=15,stepWazim=30,demID='NRCan/CDEM',elevationName='elevation',topo=False,smartsAlbedoFilePath='./SMARTS2981-PC_Package/Albedo/Albedo.txt'):
     print('open processed radiance image')
-    L,bands,fwhms,processing_metadata,metadata=atmoCorrection.getImageAndParameters(pathToRadianceImage)
+    L,bands,fwhms,processing_metadata,metadata=surehyp.atmoCorrection.getImageAndParameters(pathToRadianceImage)
 
     ####
     #get info from the processing metadata for clearer visualization in the input of the subsequent functions
@@ -98,19 +98,19 @@ def atmosphericCorrection(pathToRadianceImage,pathToOutImage,stepAltit=1,stepTil
     ####
 
     print('get haze spectrum')
-    L,Lhaze=atmoCorrection.darkObjectDehazing(L,bands)
+    L,Lhaze=surehyp.atmoCorrection.darkObjectDehazing(L,bands)
 
     print('removal of thin cirrus')
-    L,cloudMask=atmoCorrection.cirrusRemoval(bands,L,latit,doy,satelliteZenith,zenith,azimuth)
+    L,cloudMask=surehyp.atmoCorrection.cirrusRemoval(bands,L,latit,doy,satelliteZenith,zenith,azimuth)
 
     print('save cloudmask')
     np.save(pathToOutImage+'_cloudMask',cloudMask)
 
     print('get average elevation of the scene from GEE')
-    altit=atmoCorrection.getGEEdem(UL_lat,UL_lon,UR_lat,UR_lon,LL_lat,LL_lon,LR_lat,LR_lon,demID=demID,elevationName=elevationName)
+    altit=surehyp.atmoCorrection.getGEEdem(UL_lat,UL_lon,UR_lat,UR_lon,LL_lat,LL_lon,LR_lat,LR_lon,demID=demID,elevationName=elevationName)
 
     print('get atmosphere content')
-    wv,o3,flag_no_o3=atmoCorrection.getAtmosphericParameters(bands,L,datestamp1,year,doy,longit,latit,altit,satelliteZenith,zenith,azimuth)
+    wv,o3,flag_no_o3=surehyp.atmoCorrection.getAtmosphericParameters(bands,L,datestamp1,year,doy,longit,latit,altit,satelliteZenith,zenith,azimuth)
 
     if flag_no_o3==True:
         IO3=1
@@ -121,21 +121,21 @@ def atmosphericCorrection(pathToRadianceImage,pathToOutImage,stepAltit=1,stepTil
     # Atmospheric correction -- flat surface
     print('obtain radiative transfer outputs')
     #get the atmosphere parameters for the sun-ground section using the image acquisition time to determine sun angle
-    df=atmoCorrection.runSMARTS(ALTIT=altit,LATIT=latit,IMASS=0,ZENITH=zenith,AZIM=azimuth,SUNCOR=atmoCorrection.get_SUNCOR(doy),IH2O=0,WV=wv,IO3=IO3,IALT=0,AbO3=o3)
+    df=surehyp.atmoCorrection.runSMARTS(ALTIT=altit,LATIT=latit,IMASS=0,ZENITH=zenith,AZIM=azimuth,SUNCOR=surehyp.atmoCorrection.get_SUNCOR(doy),IH2O=0,WV=wv,IO3=IO3,IALT=0,AbO3=o3)
     #get the atmosphere parameters for the ground-satellite section by setting the 'sun' (in SMARTS) at the satellite zenith position to get the transmittance over the correct optical path length
-    df_gs=atmoCorrection.runSMARTS(ALTIT=altit,LATIT=0,LONGIT=0,IMASS=0,SUNCOR=atmoCorrection.get_SUNCOR(doy),ITURB=5,ZENITH=satelliteZenith,AZIM=0,IH2O=0,WV=wv,IO3=IO3,IALT=0,AbO3=o3)
+    df_gs=surehyp.atmoCorrection.runSMARTS(ALTIT=altit,LATIT=0,LONGIT=0,IMASS=0,SUNCOR=surehyp.atmoCorrection.get_SUNCOR(doy),ITURB=5,ZENITH=satelliteZenith,AZIM=0,IH2O=0,WV=wv,IO3=IO3,IALT=0,AbO3=o3)
 
     print('compute radiance to reflectance')
-    R=atmoCorrection.computeLtoR(L,bands,df,df_gs)
+    R=surehyp.atmoCorrection.computeLtoR(L,bands,df,df_gs)
 
     if topo==False:
         print('save the reflectance image')
-        atmoCorrection.saveRimage(R,metadata,pathToOutImage)
+        surehyp.atmoCorrection.saveRimage(R,metadata,pathToOutImage)
     else:
         #######################################
         #atmospheric correction -- rough terrain
         print('write Albedo.txt file for SMARTS')
-        pathToAlbedoFile=atmoCorrection.writeAlbedoFile(R,bands,pathOut=smartsAlbedoFilePath)
+        pathToAlbedoFile=surehyp.atmoCorrection.writeAlbedoFile(R,bands,pathOut=smartsAlbedoFilePath)
 
         print('get scene background reflectance')
         sp=pd.read_csv(pathToAlbedoFile,header=3,sep='\s+')
@@ -145,27 +145,27 @@ def atmosphericCorrection(pathToRadianceImage,pathToOutImage,stepAltit=1,stepTil
         rho_background=f(df['Wvlgth']*1E-3)
 
         print('download DEM images from GEE')
-        path_to_dem = atmoCorrection.getDEMimages(UL_lon,UL_lat,UR_lon,UR_lat,LR_lon,LR_lat,LL_lon,LL_lat,demID=demID,elevationName=elevationName)
+        path_to_dem = surehyp.atmoCorrection.getDEMimages(UL_lon,UL_lat,UR_lon,UR_lat,LR_lon,LR_lat,LL_lon,LL_lat,demID=demID,elevationName=elevationName)
 
         print('reproject DEM images')
-        path_to_reprojected_dem = atmoCorrection.reprojectDEM(pathToRadianceImage,path_elev=path_to_dem)
+        path_to_reprojected_dem = surehyp.atmoCorrection.reprojectDEM(pathToRadianceImage,path_elev=path_to_dem)
 
         print('resampling')
-        path_elev=atmoCorrection.matchResolution(pathToRadianceImage,path_elev=path_to_reprojected_dem)
+        path_elev=surehyp.atmoCorrection.matchResolution(pathToRadianceImage,path_elev=path_to_reprojected_dem)
 
         path_elev='C:/Users/tmiragli/Github/SUREHYP/elev/tmp_blurred.tif'
 
         print("extract the data corresponding to the Hyperion image's pixels")
-        elev, slope, wazim=atmoCorrection.extractDEMdata(pathToRadianceImage,path_elev=path_elev)
+        elev, slope, wazim=surehyp.atmoCorrection.extractDEMdata(pathToRadianceImage,path_elev=path_elev)
 
         print('computing the LUT for the rough terrain correction')
-        R=atmoCorrection.getDemReflectance(altitMap=elev,tiltMap=slope,wazimMap=wazim,stepAltit=stepAltit,stepTilt=stepTilt,stepWazim=stepWazim,latit=latit,IH2O=0,WV=wv,IO3=IO3,IALT=0,AbO3=o3,doy=doy,zenith=zenith,azimuth=azimuth,satelliteZenith=satelliteZenith,satelliteAzimuth=satelliteAzimuth,L=L,bands=bands,IALBDX=1,rho_background=rho_background)
+        R=surehyp.atmoCorrection.getDemReflectance(altitMap=elev,tiltMap=slope,wazimMap=wazim,stepAltit=stepAltit,stepTilt=stepTilt,stepWazim=stepWazim,latit=latit,IH2O=0,WV=wv,IO3=IO3,IALT=0,AbO3=o3,doy=doy,zenith=zenith,azimuth=azimuth,satelliteZenith=satelliteZenith,satelliteAzimuth=satelliteAzimuth,L=L,bands=bands,IALBDX=1,rho_background=rho_background)
 
         print('MM topography correction')
-        R=atmoCorrection.MM_topo_correction(R,bands,slope*np.pi/180,wazim*np.pi/180,zenith*np.pi/180,azimuth*np.pi/180)
+        R=surehyp.atmoCorrection.MM_topo_correction(R,bands,slope*np.pi/180,wazim*np.pi/180,zenith*np.pi/180,azimuth*np.pi/180)
 
         print('save the reflectance image')
-        atmoCorrection.saveRimage(R,metadata,pathToOutImage)
+        surehyp.atmoCorrection.saveRimage(R,metadata,pathToOutImage)
 
     return pathToOutImage
 
